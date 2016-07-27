f71f539468905d44f30e5fbbe706a501e8c4da46
Lucas Pirola <lucaspirola@gmail.com>
2016-07-27 19:01:18 -0300

    Add start point to work with silex

---
+++ src/app.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// ... definitions
+$app['debug'] = true;
+
+$app->get('/hello/{name}', function($name) use($app) {
+	    return 'Hello '.$app->escape($name);
+});
+
+$blogPosts = array(
+    1 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'igorw',
+        'title'     => 'Using Silex',
+        'body'      => '...',
+    ),
+);
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $post) {
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+return $app;
