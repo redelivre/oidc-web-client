050295d8705a06f2a9c15dd7a7ce58030d779d6a
Lucas Pirola <lucaspirola@gmail.com>
2016-07-28 17:02:22 -0300

    Add form to openid dynamic register

--- src/ClientControllerProvider.php
+++ src/ClientControllerProvider.php
@@ -12,30 +12,42 @@ class ClientControllerProvider implements ControllerProviderInterface
 {
   public function connect(Application $app)
   {
+		ini_set('CURLOPT_ACCEPTTIMEOUT_MS', 90000);
     // creates a new controller based on the default route
     $controllers = $app['controllers_factory'];
 
     $controllers->match('/', function (Request $request) use ($app) {
       // some default data for when the form is displayed the first time
       $data = array(
-        'name' => 'Your name',
-        'email' => 'Your email',
+        'client_name' => 'Name',
+				'url' => 'OpenID URL',
+        'redirect_url' => 'Redirect URL'
       );
 
       $form = $app['form.factory']->createBuilder(FormType::class, $data)
-      ->add('name')
-      ->add('email')
-      ->add('billing_plan', ChoiceType::class, array(
-        'choices' => array(1 => 'free', 2 => 'small_business', 3 => 'corporate'),
-        'expanded' => true,
-      ))
+      ->add('client_name')
+      ->add('url')
+			->add('redirect_url')
+      // ->add('billing_plan', ChoiceType::class, array(
+      //   'choices' => array(1 => 'free', 2 => 'small_business', 3 => 'corporate'),
+      //   'expanded' => true,
+      // ))
       ->getForm();
 
       $form->handleRequest($request);
 
       if ($form->isValid()) {
         $data = $form->getData();
-				$app['monolog']->addDebug($data);
+
+        $oidc = new \OpenIDConnectClient($data['url']);
+				$oidc->setRedirectURL($data['redirect_url']);
+				$oidc->setProviderURL($data['url']);
+				$oidc->setClientName($data['client_name']);
+        $oidc->register();
+        $client_id = $oidc->getClientID();
+        // $client_secret = $oidc->getClientSecret();
+
+        $app['monolog']->addDebug($client_id);
 
         // do something with the data
 
--- src/app.php
+++ src/app.php
@@ -3,35 +3,24 @@ namespace OIDCWebClient;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-// use Silex\Provider\FormServiceProvider;
-
 $app = new \Silex\Application();
+$app['debug'] = true;
+
+$app->register(new \Silex\Provider\LocaleServiceProvider());
+$app->register(new \Silex\Provider\FormServiceProvider());
+$app->register(new \Silex\Provider\ValidatorServiceProvider());
 
 $app->register(new \Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/development.log',
 ));
 
-$app->register(new \Silex\Provider\LocaleServiceProvider());
 $app->register(new \Silex\Provider\TranslationServiceProvider(), array(
     'locale_fallbacks' => array('en'),
 ));
 
-$app->register(new \Silex\Provider\FormServiceProvider());
-
-$app->register(new \Silex\Provider\ValidatorServiceProvider());
-
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/Views',
 ));
-
-$app['debug'] = true;
-
-$app->get('/hello/{name}', function($name) use($app) {
-  return $app['twig']->render('hello.twig', array(
-    'name' => $name,
-  ));
-
-});
 
 $app->mount('/', new ClientControllerProvider());
 
